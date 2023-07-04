0e8915324170d585112d071f207f8f209bcf46b9
Bob Marin <74962795+robmaasmarin@users.noreply.github.com>
2023-07-04 17:23:24 +0200

    Add files via upload

---
+++ quotes.js
@@ -0,0 +1,58 @@
+const COLDPLAY = [{
+quote: "Nobody said it was easy, no one ever said it would be this hard.",
+author: "Coldplay"},
+{quote: "You get lighter the more it gets dark.",
+author: "Coldplay"
+
+}, {
+quote: "Don’t wanna see another generation drop, I’d rather be a comma than a full stop",
+author: "Coldplay"
+}, {
+quote: "The truth is that I miss you.",
+author: "Coldplay"
+}, {
+quote: "Life goes on, it gets so heavy, the wheel breaks the butterfly.",
+author: "Coldplay"
+}, {
+quote: "You want to stop just before you begin, never give in, never give in",
+author: "Coldplay"
+},
+{
+quote: "Lights will guide you home and ignite your bones.",
+author: "Coldplay"
+}, {
+quote: "We live in a beautiful world, yeah we do, yeah we do.",
+author: "Coldplay"
+}, {
+quote: "Time seemed to say: Forget the world and its weight.",
+author: "Coldplay"
+}, {
+quote: "So come over, just be patient and don’t worry.",
+author: "Coldplay"
+}, {
+quote: "You cut me down to size and opened up my eyes, made me realize what I could not see.",
+author: "Coldplay"
+}, {
+quote: "I’m ready for the change.",
+author: "Coldplay"
+}, {
+quote: "I don’t know which way I’m going, I don’t know which way I’ve come.",
+author: "Coldplay"
+}, {
+quote: "All I know is that I love you so, so much that it hurts.",
+author: "Coldplay"
+}, {
+quote: "I’ll be ’round, I’ll be loving you always.",
+author: "Coldplay"
+}, {
+quote: "Gonna hold up half the sky and say, ‘only I own me.",
+author: "Coldplay"
+}, {
+quote: "Take the fire from my belly and the beat from my heart, still I won’t let go.",
+author: "Coldplay"
+}, {
+quote: "I know it’s over,’ Icarus says to the sun.",
+author: "Coldplay"
+}
+
+];
